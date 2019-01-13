4f7cb0ac16ff3aeac400a58d0e271bdfab993be8
Louis de Fromont <louisspaghetti@gmail.com>
2019-01-12 23:38:19 -0500

    Added first code

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,13 +1,33 @@
 ﻿using System;
+using System.IO;
 
-namespace ConsoleApp1
+namespace TestingClass
 {
     class Program
     {
         static void Main(string[] args) //main method
         {
-            Console.WriteLine("Welcome.");
+            TextReader tIn = Console.In;
+            String input = tIn.ReadLine();
+            String t = input.ToLower();
+
+            String[] verbs = new String[] { "run", "die" };
+
+            for (int x=0; x < verbs.Length; x++)
+            {
+                if (verbs[x] == t)
+                {
+                    Console.WriteLine(input);
+                    x = 100;
+                }
+                else if (x == (verbs.Length-1))
+                {
+                    Console.WriteLine("Input not recognized");
+                }
+            }
+
             Console.ReadKey();
+            
         }
     }
 }
